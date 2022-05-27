15fe2a6cb0e7c531240e5ae5815fa8d4478aa46e
Smith Bhojak <99sbhojak@gmail.com>
2022-05-27 18:06:37 +0530

    New Design

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function run(){
+    console.log('function Called')
+    $('.content').imagesLoaded({ background: true }, function() {
+        console.log('images loaded')
+        setTimeout(removeLoader, 2000);
+      });
+ }
+
+ function removeLoader(){
+    
+    let loader = document.querySelector(".loader");
+    let content = document.querySelector(".content");
+    console.log("Removing Loader");
+    loader.style.display = 'none'
+    content.style.display = 'block';
+    
+    }
+    
+function navBar() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+ }
